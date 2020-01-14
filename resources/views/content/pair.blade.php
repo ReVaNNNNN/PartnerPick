597c6397276f2f807f0be12264df84c5ea9ceab8
ReVaN <revannn5480@gmail.com>
2020-01-14 18:20:27 +0100

    Add basic blades for 3 types of algorithms.

---
+++ resources/views/content/pair.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('content')
+    <div class="inner cover">
+        <h1 class="cover-heading">@lang('index_content.body_title')</h1>
+        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+
+    </div>
+@endsection
