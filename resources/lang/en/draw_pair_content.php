8383284841094c74e19afd3da3947a619351997d
ReVaN <revannn5480@gmail.com>
2020-02-15 11:14:32 +0100

    Style for form to pair algorithm. Translation for pair algorithm.

---
+++ resources/lang/en/draw_pair_content.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'draw_name' => 'Pair',
+    'add_names' => 'Add names',
+    'give_name' => 'Give a name',
+    'name_too_short' => 'Names should contain at least 2 letters',
+    'button_add' => 'Add',
+    'button_draw' => 'Draw',
+    'list_names' => 'People',
+    'show_all_results' => 'Show all results',
+    'show_one_by_one' => 'Show name one by one',
+];
---
+++ resources/lang/pl/draw_pair_content.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'draw_name' => 'Paruj',
+    'add_names' => 'Dodaj imiona',
+    'give_name' => 'Podaj imię',
+    'name_too_short' => 'Imię powinno zawierać co najmniej 2 litery',
+    'button_add' => 'Dodaj',
+    'button_draw' => 'Losuj',
+    'list_names' => 'Osoby',
+    'show_all_results' => 'Pokaż wynik od razu',
+    'show_one_by_one' => 'Pokazuj wynik jeden po drugin',
+];
