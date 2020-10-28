ef5b8342fedd5eb51c7213e38971f84db09289e6
Kamil Sobecki <revannn5480@gmail.com>
2020-10-28 08:32:54 +0100

    Update ExampleTest.php

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -7,8 +7,6 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
      * @return void
      */
     public function testBasicTest(): void
